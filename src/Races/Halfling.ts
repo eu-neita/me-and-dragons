80afb3eb5d909964872fe28ebc557dc36409be03
eu-neita <neitass.music@gmail.com>
2023-08-22 18:51:56 -0300

    feat: halfling calsse

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,3 +1,16 @@
-class Halfling {
-  
+import Race from './Race';
+
+class Halfling extends Race {
+  private _maxLifePoints: number;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 99;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
 }
+
+export default Halfling;
